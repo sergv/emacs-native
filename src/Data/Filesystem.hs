7a79b8f590f0ac5a2eafb890e8d2534ee4925335
Sergey Vinokurov <serg.foo@gmail.com>
2023-05-06 13:24:41 +0100

    Make sure findRec passes root with trailing slash separator to filePred

--- src/Data/Filesystem.hs
+++ src/Data/Filesystem.hs
@@ -16,7 +16,6 @@ module Data.Filesystem
 
 import Control.Concurrent.Async
 import Control.Monad.Catch
-
 import Data.Foldable
 import Data.NBSem
 import System.Directory.OsPath.FileType as Streaming
@@ -48,60 +47,66 @@ findRec
   -> IO ()
 findRec followSymlinks extraJobs dirPred filePred consumeOutput roots = do
   sem <- newNBSem extraJobs
-  findWithSem sem $ reverse $ toList roots
-  where
-    findWithSem :: NBSem -> [AbsDir] -> IO ()
-    findWithSem sem ps =
-      foldr runWithRoot (pure ()) ps
-      where
-        runWithRoot :: AbsDir -> IO () -> IO ()
-        runWithRoot p' goNext = doDir p' goNext
-          where
-            doDir :: AbsDir -> IO () -> IO ()
-            doDir path processNextDir
-              | dirPred path = do
-                acquired <- tryAcquireNBSem sem
-                if acquired
-                then
-                  withAsync (goDir path `finally` releaseNBSem sem) $ \yAsync ->
-                    processNextDir *> wait yAsync
-                else goDir path *> processNextDir
-              | otherwise =
-                processNextDir
+  let runWithRoot :: AbsDir -> IO () -> IO ()
+      runWithRoot currRoot goNext = doDir currRoot goNext
+        where
+          currRootWithTrailingSep :: AbsDir
+          currRootWithTrailingSep = AbsDir $ addTrailingPathSeparator $ unAbsDir currRoot
 
-            goDir :: AbsDir -> IO ()
-            goDir d =
-              bracket
-                (Streaming.openDirStream (unAbsDir d))
-                Streaming.closeDirStream
-                (goDirStream d)
-              where
-                goDirStream :: AbsDir -> Streaming.DirStream -> IO ()
-                goDirStream (AbsDir root) stream = go
-                  where
-                    go :: IO ()
-                    go = do
-                      x <- Streaming.readDirStream stream
-                      case x of
-                        Nothing -> pure ()
-                        Just y  -> do
-                          let y' :: OsPath
-                              y' = root </> y
-                          ft <- Streaming.getFileType y'
-                          case ft of
-                            Streaming.Other        -> go
-                            Streaming.File         -> doFile (AbsFile y') *> go
-                            Streaming.FileSym      -> doFile (AbsFile y') *> go
-                            Streaming.Directory    -> doDir (AbsDir y') go
-                            Streaming.DirectorySym ->
-                              case followSymlinks of
-                                FollowSymlinks        -> doDir (AbsDir y') go
-                                ReportSymlinks report -> reportDir report (AbsDir y') *> go
+          doDir :: AbsDir -> IO () -> IO ()
+          doDir path processNextDir
+            | dirPred path = do
+              acquired <- tryAcquireNBSem sem
+              if acquired
+              then
+                withAsync (goDirRelease path) $ \yAsync ->
+                  processNextDir *> wait yAsync
+              else goDir path *> processNextDir
+            | otherwise =
+              processNextDir
 
-                doFile :: AbsFile -> IO ()
-                doFile path = do
-                  traverse_ consumeOutput =<< filePred p' path
+          goDir :: AbsDir -> IO ()
+          goDir d =
+            bracket
+              (Streaming.openDirStream (unAbsDir d))
+              Streaming.closeDirStream
+              (goDirStream d)
 
-                reportDir :: (AbsDir -> Maybe a) -> AbsDir -> IO ()
-                reportDir f path =
-                  for_ (f path) consumeOutput
+          goDirRelease :: AbsDir -> IO ()
+          goDirRelease d =
+            bracket
+              (Streaming.openDirStream (unAbsDir d))
+              (\stream -> Streaming.closeDirStream stream *> releaseNBSem sem)
+              (goDirStream d)
+
+          goDirStream :: AbsDir -> Streaming.DirStream -> IO ()
+          goDirStream (AbsDir root) stream = go
+            where
+              go :: IO ()
+              go = do
+                x <- Streaming.readDirStream stream
+                case x of
+                  Nothing -> pure ()
+                  Just y  -> do
+                    let y' :: OsPath
+                        y' = root </> y
+                    ft <- Streaming.getFileType y'
+                    case ft of
+                      Streaming.Other        -> go
+                      Streaming.File         -> doFile (AbsFile y') *> go
+                      Streaming.FileSym      -> doFile (AbsFile y') *> go
+                      Streaming.Directory    -> doDir (AbsDir y') go
+                      Streaming.DirectorySym ->
+                        case followSymlinks of
+                          FollowSymlinks        -> doDir (AbsDir y') go
+                          ReportSymlinks report -> reportDir report (AbsDir y') *> go
+
+          doFile :: AbsFile -> IO ()
+          doFile path = do
+            traverse_ consumeOutput =<< filePred currRootWithTrailingSep path
+
+          reportDir :: (AbsDir -> Maybe a) -> AbsDir -> IO ()
+          reportDir f path =
+            for_ (f path) consumeOutput
+
+  foldr runWithRoot (pure ()) roots
