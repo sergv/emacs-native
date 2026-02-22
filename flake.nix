baef339a9517d576dac41e007542128bc3afd7ef
Sergey Vinokurov <serg.foo@gmail.com>
2026-02-22 01:02:53 +0000

    Bump flakes to use nixpkgs from registry. Make flakes provide the ‘ghc-pie’ executable now named as default ‘ghc’ with pie enabled on the inside

--- flake.nix
+++ flake.nix
@@ -2,24 +2,40 @@
   inputs = {
 
     nixpkgs = {
-      url = "github:nixos/nixpkgs";
+      url = "nixpkgs";
     };
 
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
 
+    haskellNix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-unstable.follows = "nixpkgs";
+    };
+
+    haskell-nixpkgs-improvements = {
+      url = "github:sergv/haskell-nixpkgs-improvements" ;
+
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-unstable.follows = "nixpkgs";
+      inputs.haskellNix.follows = "haskellNix";
+    };
+
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, haskellNix, haskell-nixpkgs-improvements }:
     flake-utils.lib.eachSystem ["x86_64-linux" "i686-linux"] (system:
       let pkgs = nixpkgs.legacyPackages.${system};
+          ghc = haskell-nixpkgs-improvements.haskell-package-sets."${system}".host.ghc914-pie.ghc;
       in {
         devShell = pkgs.mkShell {
           nativeBuildInputs = [
             pkgs.rure
             pkgs.rure.dev
             pkgs.pkg-config
+            ghc
           ];
           LD_LIBRARY_PATH = "${pkgs.rure}/lib";
           # shellHook = ''
